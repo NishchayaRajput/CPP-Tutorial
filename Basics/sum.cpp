f6f0e3d6f7c347e160e746cf52c821dd951a963e
Nishchaya Rajput <114467750+NishchayaRajput@users.noreply.github.com>
2023-03-07 15:38:48 +0530

    Add files via upload

---
+++ Basics/sum.cpp
@@ -0,0 +1,16 @@
+// This is a C++ Program to add two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int first_integer, second_integer, sum;
+    cout<<"Enter First integer"<<endl;
+    cin>>first_integer;
+    cout<<"Enter Second integer"<<endl;
+    cin>>second_integer;
+    sum=first_integer+second_integer;
+    cout<<sum;
+
+
+}
+
